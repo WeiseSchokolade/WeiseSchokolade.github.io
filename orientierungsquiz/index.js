2a2e388505c44db64161be8c28f8ec39380f75f6
WeiseSchokolade <99491985+WeiseSchokolade@users.noreply.github.com>
2025-02-02 17:47:53 +0100

    Changed results page to coordinate system and added FAQ

--- orientierungsquiz/index.js
+++ orientierungsquiz/index.js
@@ -116,23 +116,24 @@ function showSummary() {
     `;
     const responsesDiv = document.querySelector("#responses");
     for (let i = 0; i < questions.length; i++) {
+        const questionId = i;
         responsesDiv.innerHTML += `
             <div class="summaryItem">
-                <div class="summaryQuestion">${questions[i]}</div>
+                <div class="summaryQuestion">${questions[questionId]}</div>
                 <div class="summaryResponse">
-                    <select class="summarySelect" id="responseSelect${i}">
-                        <option value="2" ${responses[i] == 2 ? "selected" : ""}>Stimme voll zu</option>
-                        <option value="1" ${responses[i] == 1 ? "selected" : ""}>Stimme eher zu</option>
-                        <option value="0" ${responses[i] == 0 ? "selected" : ""}>Unsicher</option>
-                        <option value="-1" ${responses[i] == -1 ? "selected" : ""}>Stimme eher nicht zu</option>
-                        <option value="-2" ${responses[i] == -2 ? "selected" : ""}>Stimme gar nicht zu</option>
+                    <select class="summarySelect" id="responseSelect${questionId}">
+                        <option value="2" ${responses[questionId] == 2 ? "selected" : ""}>Stimme voll zu</option>
+                        <option value="1" ${responses[questionId] == 1 ? "selected" : ""}>Stimme eher zu</option>
+                        <option value="0" ${responses[questionId] == 0 ? "selected" : ""}>Unsicher</option>
+                        <option value="-1" ${responses[questionId] == -1 ? "selected" : ""}>Stimme eher nicht zu</option>
+                        <option value="-2" ${responses[questionId] == -2 ? "selected" : ""}>Stimme gar nicht zu</option>
                     </select>
                 </div>
             <div>
         `;
-        let select = document.querySelector("#responseSelect" + i);
+        let select = document.querySelector("#responseSelect" + questionId);
         select.onclick = () => {
-            responses[i] = select.value;
+            responses[questionId] = select.value;
         };
     }
     document.querySelector("#proceedBtn2").onclick = () => {
@@ -152,32 +153,106 @@ function showResults() {
             <div>Ergebnis</div>
         </div>
         <div class="results">
-            <div class="resultStat">
-                <div class="results_title">
-                    Werteorientierung
+            <div class="resultCoord">
+                <div>
+                    konservativ
                 </div>
-                <div class="resultContainer">
-                    <div>Liberal</div>
-                    <div class="resultSlider">
-                        <div class="resultThumb" style="left: ${(-werteorientierung + questions.length) / (questions.length * 2) * 95}%"></div>
+                <div class="resultCanvasContainer">
+                    <div style="text-align: right;">
+                        Links / sozialistisch
                     </div>
-                    <div>Konservativ</div>
+                    <canvas id="resultsCanvas"></canvas>
+                    <div>
+                        Rechts / kapitalistisch
+                    </div>
+                </div>
+                <div>
+                    liberal
                 </div>
             </div>
-            <div class="resultStat">
-                <div class="results_title">
-                    Wirtschaftliche/Soziale Grundordnung
-                </div>
-                <div class="resultContainer">
-                <div>Liberal</div>
-                    <div class="resultSlider">
-                        <div class="resultThumb" style="left: ${(-wirtschaft + questions.length) / (questions.length * 2) * 95}%"></div>
-                    </div>
-                    <div>Sozial</div>
-                </div>
+            <div>
+                <h1>FAQ</h1>
+                <h3>Woher kommen die Fragen?</h3>
+                <span>
+                    Die Fragen wurden basierend auf dem Fragenkatalog von <a href="https://www.politnavi.de/">https://www.politnavi.de/</a> erstellt. 
+                </span>
+                <h3>Was fange ich mit meinem Ergebnis an?</h3>
+                <span>
+                    Politische Orientierung ist komplexer, als dass sie auf zwei Achsen dargestellt werden könnte. Ein kurzes Online-Quiz bricht komplexe
+                    Themen auf vereinfachte Fragen herunter. Wie du vielleicht gemerkt hast, bräuchte man für all diese Fragen viel mehr Antwortmöglichkeiten -
+                    von "Stimme zu, aber nur unter der Bedingung, dass..." bis hin zu "Stimme nicht zu, aber nur, weil ..." kann alles dabei sein. Es ist somit
+                    wichtig, sich für die Wahl einer Partei die Parteiprogramme und die Kandidaten der Parteien genauer anzuschauen. Die Ergebnisse dieses Quiz
+                    können zumindest dazu verwendet werden, einzuschränken, welche Parteiprogramme man sich überhaupt anschauen möchte. 
+                </span>
+                <h3>Ich habe ein Ergebnis erhalten, das nicht zu mir passt.</h3>
+                <span>
+                    Das kann schon mal passieren! Das kann daran liegen, dass die Fragen, die dir wichtig sind, in diesem Quiz nicht enthalten sind.
+                    Denke mal darüber nach! Wenn du zu unserem Stand kommst, können wir auch noch mehr darüber sprechen, welche Parteien dir gefallen könnten.
+                </span>
+                <h3>Werden meine Daten gespeichert?</h3>
+                <span>
+                    Nein! Sobald die Seite neu geladen wird, sind deine Daten gelöscht.
+                </span>
             </div>
         </div>
     `;
+    const resultsCanvas = document.getElementById("resultsCanvas");
+    resultsCanvas.width = resultsCanvas.clientWidth;
+    resultsCanvas.height = resultsCanvas.clientHeight;
+    drawResults(resultsCanvas, resultsCanvas.width, resultsCanvas.height, werteorientierung, wirtschaft);
+    window.onresize = () => {
+        resultsCanvas.width = resultsCanvas.clientWidth;
+        resultsCanvas.height = resultsCanvas.clientHeight;
+        drawResults(resultsCanvas, resultsCanvas.width, resultsCanvas.height, werteorientierung, wirtschaft);
+    }
+}
+
+function drawCoordinateSystem(ctx, width, height) {
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.moveTo(0, height / 2);
+    ctx.lineTo(width, height / 2);
+    ctx.moveTo(width / 2, 0);
+    ctx.lineTo(width / 2, height);
+    ctx.stroke();
+}
+
+function drawResults(canvas, width, height, werteorientierung, wirtschaft) {
+    const diagonal = Math.sqrt(width * width + height * height);
+    console.log(width, height, diagonal);
+    let ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, width, height);
+    drawCoordinateSystem(ctx, width, height);
+    
+	ctx.beginPath();
+	ctx.arc(
+        (-werteorientierung + questions.length) / (questions.length * 2) * (width - 10) + 5,
+        (wirtschaft + questions.length) / (questions.length * 2) * (height - 10) + 5, 5, 0, 2 * Math.PI);
+	ctx.fill();
+
+    //drawParty(ctx, width, height, "SPD", "#FF0000", diagonal * 0.05, 5, 3);
+}
+
+function drawParty(ctx, width, height, label, color, radius, werteorientierung, wirtschaft) {
+	ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color + "A0";
+    let centerX = (-werteorientierung + questions.length) / (questions.length * 2) * (width - radius - 2) + radius + 2;
+    let centerY = (wirtschaft + questions.length) / (questions.length * 2) * (height - radius - 2) + radius + 2;
+	ctx.arc(
+        centerX,
+        centerY, radius, 0, 2 * Math.PI);
+	ctx.fill();
+    ctx.stroke();
+
+    let labelMeasurements = ctx.measureText(label);
+    ctx.fillStyle = "#FFFFFF"
+    ctx.fillText(label, centerX - labelMeasurements.width / 2, centerY + (labelMeasurements.actualBoundingBoxAscent + labelMeasurements.actualBoundingBoxDescent) / 2)
+}
+
+function drawText() {
+    let labelMeasurements = ctx.measureText(label);
+    ctx.fillText(label, centerX - labelMeasurements.width / 2, centerY + (labelMeasurements.actualBoundingBoxAscent + labelMeasurements.actualBoundingBoxDescent) / 2)
 }
 
 showQuestionDisplay();
